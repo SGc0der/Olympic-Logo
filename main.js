3e82607c37b40e220a7adef18798bb89883ec817
sC0der <102285034+SGc0der@users.noreply.github.com>
2023-06-23 21:32:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 1;
+ctx.rect(100, 100, 600, 400);
+ctx.stroke();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 3;
+ctx.arc(250,210,40,0,2 * Math.PI);
+ctx.stroke();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 3;
+ctx.arc(350,210,40,0,2 * Math.PI);
+ctx.stroke();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 3;
+ctx.arc( 450,210,40,0,2 * Math.PI);
+ctx.stroke();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 3;
+ctx.arc(300,250,40,0,2 * Math.PI     );
+ctx.stroke()
+ctx.strokeStyle = "green";
+ctx.lineWidth = 3;
+ctx.arc(400,250,40,0,2 * Math.PI);
+ctx.stroke();
